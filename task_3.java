c327577e70d39341a960db7b6dcd0870ea35977a
Assem Prokhorova <iasem@ya.ru>
2024-10-11 14:54:12 +0500

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,31 @@
+// Напиши аннотацию для параметризованных тестов
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;
+  }
+
+@Parameterized.Parameters
+  public static Object[][] getTextData() {
+	return new Object[][] {
+        {18, true},
+        {19, true},
+        {17, false},
+        {5, false},
+    };
+  }
+
+@Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+	// Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("результаты должны совпадать", result, isAdult);
+	}
+}
